cad56eba00fd3cae0f98afc087750c759c4ef55d
Slavomir <ext-slavomir.1.hustaty@nokia.com>
2012-04-10 23:44:23 +0200

    preload

--- Alergia.sk/gen/com/hustaty/android/alergia/R.java
+++ Alergia.sk/gen/com/hustaty/android/alergia/R.java
@@ -12,12 +12,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int tree=0x7f020001;
     }
     public static final class id {
         public static final int container=0x7f050000;
+        public static final int image=0x7f050001;
+        public static final int progressBar=0x7f050003;
+        public static final int textView=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int preload=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- Alergia.sk/src/com/hustaty/android/alergia/AlergiaskActivity.java
+++ Alergia.sk/src/com/hustaty/android/alergia/AlergiaskActivity.java
@@ -3,7 +3,9 @@ package com.hustaty.android.alergia;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
@@ -56,6 +58,10 @@ public class AlergiaskActivity extends Activity {
 
 	private boolean gotGPSfix = false;
 	
+	
+	//A ProgressDialog object  
+    private ProgressDialog progressDialog;  
+    
 	/** 
 	 * Called when the activity is first created. 
 	 * 
@@ -63,25 +69,90 @@ public class AlergiaskActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-
-		this.container = (RelativeLayout) findViewById(R.id.container);
-
-		AlergyLocationService locationService = new AlergyLocationService(this);
-
-		District district = locationService.getDistrictFromLastAddress();
-
-		if(district != null && !gotGPSfix) {
-			this.gotGPSfix = true;
-			currentCounty = district.getCounty();
-			currentDistrict = district;
-			currentAlergene = Alergene.OVERALL;
-			depthLevel = Level.ALERGENE;
-		}
+//		setContentView(R.layout.main);
+	
+		//Initialize a LoadViewTask object and call the execute() method  
+        new LoadViewTask().execute();         
+  
 		
-		initUI(null);
+
+//		this.container = (RelativeLayout) findViewById(R.id.container);
+//		initUI(null);
 	}
 
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	//To use the AsyncTask, it must be subclassed  
+    private class LoadViewTask extends AsyncTask<Void, Integer, Void> {  
+        //Before running code in separate thread  
+        @Override  
+        protected void onPreExecute() {  
+            progressDialog = ProgressDialog.show(AlergiaskActivity.this,"Loading...",  
+            	    "Loading application, please wait...", false, false); 
+            
+            AlergyLocationService locationService = new AlergyLocationService(AlergiaskActivity.this);
+
+    		District district = locationService.getDistrictFromLastAddress();
+
+    		if(district != null && !gotGPSfix) {
+    			AlergiaskActivity.this.gotGPSfix = true;
+    			currentCounty = district.getCounty();
+    			currentDistrict = district;
+    			currentAlergene = Alergene.OVERALL;
+    			depthLevel = Level.ALERGENE;
+    		}
+        }  
+  
+        //The code to be executed in a background thread.  
+        @Override  
+        protected Void doInBackground(Void... params) {  
+            /* This is just a code that delays the thread execution 4 times, 
+             * during 850 milliseconds and updates the current progress. This 
+             * is where the code that is going to be executed on a background 
+             * thread must be placed. 
+             */  
+            try {  
+                //Get the current thread's token  
+                synchronized (this) {  
+                    //Initialize an integer (that will act as a counter) to zero  
+                    int counter = 0;  
+                    //While the counter is smaller than four  
+                    while(counter <= 4) {  
+                        //Wait 850 milliseconds  
+                        this.wait(850);  
+                        //Increment the counter  
+                        counter++;  
+                        //Set the current progress.  
+                        //This value is going to be passed to the onProgressUpdate() method.  
+                        publishProgress(counter*25);  
+                    }  
+                }  
+            } catch (InterruptedException e) {  
+                e.printStackTrace();  
+            }  
+            return null;  
+        }  
+  
+        //Update the progress  
+        @Override  
+        protected void onProgressUpdate(Integer... values) {  
+            //set the current progress of the progress dialog  
+            //progressDialog.setProgress(values[0]);  
+        }  
+  
+        //after executing the code in the thread  
+        @Override  
+        protected void onPostExecute(Void result) {  
+            //close the progress dialog  
+            progressDialog.dismiss();  
+            //initialize the View  
+            setContentView(R.layout.main); 
+            AlergiaskActivity.this.container = (RelativeLayout) findViewById(R.id.container);
+            initUI(null);
+        }  
+    }  
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
 	/**
 	 * OnConfigurationChange.
 	 */
